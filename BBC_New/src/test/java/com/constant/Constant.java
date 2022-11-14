3d15b916b2fd9a0ac6b778ee9d2c3055d4b798b6
Kavitha Hariharan <kavi21hari@gmail.com>
2022-11-14 11:35:53 +0000

    test

--- BBC_New/src/test/java/com/constant/Constant.java
+++ BBC_New/src/test/java/com/constant/Constant.java
@@ -1,14 +1,9 @@
 package com.constant;
 
 public class Constant {
-
-
 	public static String  APP_URL;
 	public static String  BROWSER;
 	public static String  PAGETITLE;
 	public static String SOUNDPAGETITLE;
 	public static String CATEGORYPAGETITLE;
-	
-
-
 }
